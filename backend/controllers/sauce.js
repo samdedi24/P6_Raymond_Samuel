22cef13aef040d6b03d6f5e16ad1cbec04a80e97
Samuel Raymond <samraymond24@icloud.com>
2020-08-26 16:04:24 +0200

    Ajout multer
    Ajout de multer, il est maintenant possible d'ajouter une sauces de la modifier et de la supprimer

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,15 +1,17 @@
 const Sauce = require('../models/sauce')
-
-exports.getOneSauce = (req, res, next) => {
-    Sauce.findOne({ _id: req.params.id })
-        .then(sauce => res.status(200).json(sauce))
-        .catch(error => res.status(404).json({ error }));
-};
+const fs = require('fs');
+const sauce = require('../models/sauce');
 
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
         .catch(error => res.status(400).json({ error }));
+};
+
+exports.getOneSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+        .then(sauce => res.status(200).json(sauce))
+        .catch(error => res.status(404).json({ error }));
 };
 
 exports.createSauce = (req, res, next) => {
@@ -26,16 +28,28 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-    Sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id})
-        .then(() => res.status(200).json({ message: 'Sauce modifiée' }))
-        .catch(error => res.status(400).json({ error }));
-};
+    const sauceObject = req.file ?
+      {
+        ...JSON.parse(req.body.sauce),
+        imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      } : { ...req.body };
+    Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+      .then(() => res.status(200).json({ message: 'Sauce modifiée !'}))
+      .catch(error => res.status(400).json({ error }));
+  };
 
-exports.deleteSauce = (req, res, next) => {
-    Sauce.deleteOne({ _id: req.params.id })
-       .then(() => res.status(200).json({ message: 'Sauce supprimée'}))
-       .catch(error => res.status(400).json({ error }));
-};
+  exports.deleteSauce = (req, res, next) => {
+    Sauce.findOne({ _id: req.params.id })
+      .then(sauce => {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Sauce supprimée !'}))
+            .catch(error => res.status(400).json({ error }));
+        });
+      })
+      .catch(error => res.status(500).json({ error }));
+  };
 
 
 
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const sauceCtrl = require ('../controllers/sauce');
 const auth = require('../middleware/auth');
+const multer = require('../middleware/multer-config');
 
-router.post('/', auth, sauceCtrl.createSauce);
+const sauceCtrl = require ('../controllers/sauce');
+
 router.get('/', auth, sauceCtrl.getAllSauces);
 router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
 router.delete('/:id', auth, sauceCtrl.deleteSauce);
 router.put('/:id', auth, sauceCtrl.modifySauce);
 
